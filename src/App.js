6b1fe0d8a868de88bb2d60e9f1f29ed360276229
donny.tri <donny.tri88@gmail.com>
2022-07-02 17:57:44 +0700

    Change App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello React
+        Hello React 02 July 2022
       </header>
     </div>
   );
