42794741717810d456265d3c25af9601615852e4
donny.tri <donny.tri88@gmail.com>
2022-07-04 11:26:13 +0700

    Change package.json and Time

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-          Hello React 04 July 2022, 11:15  
+          Hello React 04 July 2022, 11:25
       </h1>
     </div>
   );
