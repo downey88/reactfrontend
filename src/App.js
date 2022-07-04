7eaca4c9347ff279122600caf38d66f1347fba54
donny.tri <donny.tri88@gmail.com>
2022-07-04 13:28:45 +0700

    Change Time

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-          Hello React 04 July 2022, 11:25
+          Hello React 04 July 2022, 13:24
       </h1>
     </div>
   );
