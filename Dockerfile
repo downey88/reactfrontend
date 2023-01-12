457e117b5bb07edcf0b31d0038add1d3ae458379
donny.tri <donny.tri88@gmail.com>
2023-01-12 10:57:13 +0700

    Update Repo

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM hoosin/alpine-nginx-nodejs:latest
+
+ENV HOME=/home/app
+
+COPY . $HOME/
+
+WORKDIR $HOME/
+
+RUN npm install --silent --progress=false
+
+RUN npm run build
+
+RUN cp $HOME/build/* /usr/share/nginx/html/ -R
+
+EXPOSE 3000
--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import logo from './gitlab.svg';
+import './App.css';
+
 function App() {
   return (
-    <div>
-        <h1>Hello React 2 August 2022 19:24</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://gitlab.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Testing My Robin, 02 Januari 2023, 10:43
+        </a>
+      </header>
     </div>
   );
 }
